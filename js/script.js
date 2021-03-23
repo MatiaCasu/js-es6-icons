88478a778cf5d8cbf18397285379e5ca29d602c9
Matia <matia.casu@gmail.com>
2021-03-23 16:14:04 +0100

    Stampati oggetti di Icons con layout html
    con ciclo for e ciclo forEach

---
+++ js/script.js
@@ -0,0 +1,149 @@
+// Milestone 2
+// Coloriamo le icone per tipo
+//
+// Milestone 3
+// Creiamo una select con i tipi di icone e usiamola per filtrare le icone
+
+const icons = [
+  {
+    name: 'apple-alt',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "food"
+  },
+  {
+    name: 'ice-cream',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "food"
+  },
+  {
+    name: 'fish',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "food"
+  },
+  {
+    name: 'lemon',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "food"
+  },
+  {
+    name: 'hamburger',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "food"
+  },
+  {
+    name: 'pizza-slice',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "food"
+  },
+  {
+    name: 'beer',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "beverage"
+  },
+  {
+    name: 'glass-whiskey',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "beverage"
+  },
+  {
+    name: 'wine-bottle',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "beverage"
+  },
+  {
+    name: 'cocktail',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "beverage"
+  },
+  {
+    name: 'coffee',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "beverage"
+  },
+  {
+    name: 'glass-martini',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "beverage"
+  },
+  {
+    name: 'dragon',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "animal"
+  },
+  {
+    name: 'kiwi-bird',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "animal"
+  },
+  {
+    name: 'frog',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "animal"
+  },
+  {
+    name: 'hippo',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "animal"
+  },
+  {
+    name: 'otter',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "animal"
+  },
+  {
+    name: 'horse',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "animal"
+  },
+];
+
+// Milestone 1
+// Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
+//
+
+// Milestone 1 con ciclo for
+// const hook = $(".icons");
+// var layoutHtml = "";
+//
+// for (var i = 0; i < icons.length; i++) {
+//   layoutHtml += `
+//   <div>
+//     <i class="${icons[i].family} ${icons[i].prefix}${icons[i].name}"></i>
+//     <div class="title">${icons[i].name}</div>
+//   </div>`;
+// }
+// hook.append(layoutHtml);
+// /Milestone 1 con ciclo for
+
+
+// Milestone 1 con ciclo forEach
+const hook = $(".icons");
+let layoutHtml = "";
+icons.forEach((item) => {
+    let {name, family, prefix} = item;
+    layoutHtml +=`
+<div>
+  <i class="${family} ${prefix}${name}"></i>
+  <div class="title">${name}</div>
+</div>`;
+});
+hook.append(layoutHtml);
+// /Milestone 1 con ciclo forEach
